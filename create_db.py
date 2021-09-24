9b66afd65a1cf4ea8cd6467e4cc801c0f9afa7a1
Pablo Ponzio <pponzio@dc.exa.unrc.edu.ar>
2021-09-24 03:51:21 -0300

    Removing redudant script for handling db creation

--- create_db.py
+++ create_db.py
@@ -1,5 +1,8 @@
-from app import db
+from app import db, create_app
 
 if __name__ == "__main__":
     print("Creating DB tables")
     db.create_all()
+    # Comentar la linea anterior y descomentar la linea que sigue este si se usa una factory para crear la app
+    # db.create_all(app=create_app())
+    
--- create_db_factory.py
+++
@@ -1,5 +0,0 @@
-from app import db, create_app
-
-if __name__ == "__main__":
-    print("Creating DB tables")
-    db.create_all(app=create_app())
